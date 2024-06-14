f25cea2dc75f854a4b0ed99aeadc59bdedc56620
NguyenXuantiendung <101032317+Dunglailaptop@users.noreply.github.com>
2024-06-14 13:22:48 +0700

    first commit

---
+++ ToolTestData/ToolTestData/Program.cs
@@ -0,0 +1,19 @@
+using ToolTestData.View.Menu;
+
+namespace ToolTestData
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new RibbonForm());
+        }
+    }
+}
